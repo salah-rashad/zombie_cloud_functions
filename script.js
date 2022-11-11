42f2a79443e59c60da1e008bc1696e7dbb90a730
Salah Rashad <sla7.r4ad@gmail.com>
2022-11-11 03:45:03 +0200

    test

--- script.js
+++ script.js
@@ -36,45 +36,8 @@ handlers.getBaseCharacters = function (args) {
     return options;
 }
 
-// handlers.createUserCharacter = function (args) {
+handlers.createUserCharacter = function (args, type) {
+    var a = handlers.getBaseCharacters();
 
-//     var result = server.GetTitleData({
-//         Keys: ["Characters", "Abilities"],
-//     });
-
-//     var characters = result.Data["Characters"];
-//     var abilities = result.Data["Abilities"];
-
-//     var parsedCharacters = JSON.parse(characters);
-//     var parsedAbilities = JSON.parse(abilities);
-
-//     // log.info(parsedCharacters);
-//     // log.info(parsedAbilities);
-
-//     var engineer = parsedCharacters[2];
-//     // log.info(engineer);
-
-//     var options = {};
-
-//     for (const key in parsedCharacters) {
-//         const chr = parsedCharacters[key];
-//         var chrAbilities = chr.abilities;
-
-//         if (chrAbilities) {
-//             for (const ab in chrAbilities) {
-//                 var abID = ab.id;
-
-//                 log.info(abID);
-
-//                 let baseAbility = parsedAbilities.find(el => el.value.id == abID)
-//                 log.info(baseAbility);
-//             }
-//         }
-
-//         var id = chr.id;
-//         // log.info(chr.name);
-//         options[id] = chr;
-//     }
-
-//     return options;
-// }
+    return a;
+}
